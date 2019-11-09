0c938c4e0c2b7aa51263c67c7bc4e8ee09d1b47d
Roey Shmool <roey.shmool@gmail.com>
2019-11-09 12:25:12 -0600

    add addNewEmployeeFunction

--- client.js
+++ client.js
@@ -1,6 +1,40 @@
 console.log('JS');
+
+$(document).ready(onReady);
+
 //The application should have an input form that collects employee first name, last name, 
 //ID number, job title, annual salary.
+
+//Make array
+let employees = [];
+
+// Take input from the form & put into array
+function addNewEmployee(event) {
+    console.log('in addNewEmployee');
+    // prevents page from refreshing using form tags
+    event.preventDefault();
+    //get info from form
+    let newEmployee = {
+        first: $('#in-first').val(),
+        last: $('#in-last').val(),
+        id: $('#in-id').val(),
+        title: $('#in-title').val(),
+        Salary: $('#in-salary').val()
+    } //end object
+    // make an object into array
+    employees.push(newEmployee);
+    console.log('array:', employees);
+    
+    $('#in-first').val(''),
+    $('#in-last').val(''),
+    $('#in-id').val(''),
+    $('#in-title').val(''),
+    $('#in-salary').val('')
+
+
+    showMovies(movies);
+
+}    
 
 //A 'Submit' button should collect the form information, store the information to calculate
 // monthly costs, append information to the DOM and clear the input fields.Using the stored 
